a8ec9b4bae72c2f94ec61a3d53d583d3658eb8f9
Italo Fernandes <italojafernandes@hotmail.com>
2020-12-13 20:17:18 -0300

    tost component

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,15 @@ import SingIn from './pages/SignIn';
 // import SingUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
-import { AuthProvider } from './context/AuthContext';
+import ToastContainer from './components/ToastContainer';
+import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
   <>
     <AuthProvider>
       <SingIn />
     </AuthProvider>
+    <ToastContainer />
     <GlobalStyle />
   </>
 );
