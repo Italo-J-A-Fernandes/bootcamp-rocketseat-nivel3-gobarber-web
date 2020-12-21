fdadbe8aad47bc470ad6d5d5e0567bda10bf72c0
Italo Fernandes <italojafernandes@hotmail.com>
2020-12-20 23:48:13 -0300

    authenticated routes

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -20,7 +20,7 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>(() => {
     const token = localStorage.getItem('@GoBarber:token');
-    const user = localStorage.getItem('@GoBarber:token');
+    const user = localStorage.getItem('@GoBarber:user');
 
     if (token && user) {
       return { token, user: JSON.parse(user) };
